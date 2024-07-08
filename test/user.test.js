6cb6b40074e203d8a43b20ce07acbd492b806d9e
Paula Resende <bellsuo@gmail.com>
2024-07-08 19:28:03 +0200

    test: enhance test listing users

--- test/user.test.js
+++ test/user.test.js
@@ -6,7 +6,8 @@ test('it should list all users', () => {
     return request(app).get('/users')
         .then((res) => {
             expect(res.status).toBe(200);
-            expect(res.body).toHaveLength(1)
+            expect(res.body).toHaveLength(1);
             expect(res.body[0]).toHaveProperty('name', 'John Doe');
+            expect(res.body[0]).toHaveProperty('email', 'john@email.com');
         });
 });
